b2d5ca99d485ae2297cf87d1849f96572715ad46
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-04-07 15:05:28 -0400

    upto part 3 done

---
+++ answers/exercise2.sql
@@ -0,0 +1,1 @@
+mysql> DROP SCHEMA myNewDB;
---
+++ answers/exercise3.sql
@@ -0,0 +1,7 @@
+mysql> CREATE SCHEMA myDB;
+mysql> CREATE TABLE myDB.Users (
+    -> UserID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    -> LastName VARCHAR(255),
+    -> FirstName VARCHAR(255),
+    -> Address VARCHAR(255),
+    -> City VARCHAR(255));
